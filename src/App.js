937d02d59347af5cd64859689800539fbdf6826f
kazuki <kaz_cl_sys@icloud.com>
2021-04-14 10:12:02 +0900

    to Functional Component

--- src/App.js
+++ src/App.js
@@ -1,161 +1,154 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pickMethod: '病院',
-      hospitalName: 'A病院',
-      petName: '',
-      tubeNum: "1本",
-      name: '',
-      address: '',
-      email: '',
-      phone: '',
-      submitDisabled: true
-    };
+const App = () => {
+  const defaultState = {
+    pickMethod: '病院',
+    hospitalName: 'A病院',
+    petName: '',
+    tubeNum: "1本",
+    name: '',
+    address: '',
+    email: '',
+    phone: '',
+    submitDisabled: true
+  };
+  const [state, setState] = useState(defaultState);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  
-  isFormValid = (state) => {
+  const isFormValid = (state) => {
     const {petName, name, address, email, phone} = state
     return petName && name && address && email && phone
   }
 
-  handleChange(event) {
+  const handleChange = (event) => {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
 
-    this.setState({
+    setState({
       [name]: value
     });
-    this.setState((prevState) => ({
-      submitDisabled: !this.isFormValid(prevState)
+    setState((prevState) => ({
+      submitDisabled: !isFormValid(prevState)
     }));
   }
 
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     window.open("https://dna4x.official.ec/items/41519433");
     event.preventDefault();
   }
 
-  render() {
-    return (
-      <form id="main-form" onSubmit={this.handleSubmit}>
-        <label>
-          採取方法:
+  return (
+    <form id="main-form" onSubmit={handleSubmit}>
+      <label>
+        採取方法:
         <select
+          name="pickMethod"
+          value={state.pickMethod}
+          onChange={handleChange}>
+          <option value="病院">病院</option>
+          <option value="ご自身">ご自身</option>
+        </select>
+      </label>
+
+      {
+        (state.pickMethod === '病院') &&
+        <label>
+          病院名:
+          <select
             name="pickMethod"
-            value={this.state.pickMethod}
-            onChange={this.handleChange}>
-            <option value="病院">病院</option>
-            <option value="ご自身">ご自身</option>
+            value={state.hospitalName}
+            onChange={handleChange}>
+            <option value="A病院">A病院</option>
+            <option value="B病院">B病院</option>
           </select>
         </label>
+      }
 
-        {
-          (this.state.pickMethod === '病院') &&
-          <label>
-            病院名:
-            <select
-              name="pickMethod"
-              value={this.state.hospitalName}
-              onChange={this.handleChange}>
-              <option value="A病院">A病院</option>
-              <option value="B病院">B病院</option>
-            </select>
-          </label>
-        }
+      <label>
+        ペットのお名前:
+        <input
+          name="petName"
+          type="text"
+          value={state.petName}
+          onChange={handleChange} />
+      </label>
 
-        <label>
-          ペットのお名前:
-          <input
-            name="petName"
-            type="text"
-            value={this.state.petName}
-            onChange={this.handleChange} />
-        </label>
-
-        <label>
-          申込チューブの本数:
+      <label>
+        申込チューブの本数:
         <select
-            name="tubeNum"
-            value={this.state.tubeNum}
-            onChange={this.handleChange}>
-            <option value="1本">1本</option>
-            <option value="2本">2本</option>
-            <option value="3本">3本</option>
-            <option value="4本">4本</option>
-          </select>
-        </label>
+          name="tubeNum"
+          value={state.tubeNum}
+          onChange={handleChange}>
+          <option value="1本">1本</option>
+          <option value="2本">2本</option>
+          <option value="3本">3本</option>
+          <option value="4本">4本</option>
+        </select>
+      </label>
 
-        <label>
-          氏名:
-          <input
-            name="name"
-            type="text"
-            value={this.state.name}
-            onChange={this.handleChange} />
-        </label>
-
-
-        <label>
-          住所:
-          <input
-            name="address"
-            type="text"
-            value={this.state.address}
-            onChange={this.handleChange} />
-        </label>
+      <label>
+        氏名:
+        <input
+          name="name"
+          type="text"
+          value={state.name}
+          onChange={handleChange} />
+      </label>
 
 
-        <label>
-          メールアドレス:
-          <input
-            name="email"
-            type="電話番号"
-            value={this.state.email}
-            onChange={this.handleChange} />
-        </label>
-
-        <label>
-          電話番号:
-          <input
-            name="phone"
-            type="tel"
-            value={this.state.phone}
-            onChange={this.handleChange} />
-        </label>
+      <label>
+        住所:
+        <input
+          name="address"
+          type="text"
+          value={state.address}
+          onChange={handleChange} />
+      </label>
 
 
-        <div id="note">
-          <ul>
-            <li>採取方法: {this.state.pickMethod}</li>
-            {
-              (this.state.pickMethod === '病院') && 
-              <li>病院名: {this.state.hospitalName}</li>
-            }
-            <li>ペットのお名前: {this.state.petName}</li>
-            <li>申込チューブの本数: {this.state.tubeNum}</li>
-            <li>氏名: {this.state.name}</li>
-            <li>住所: {this.state.address}</li>
-            <li>メールアドレス: {this.state.email}</li>
-            <li>電話番号: {this.state.phone}</li>
-          </ul>
+      <label>
+        メールアドレス:
+        <input
+          name="email"
+          type="電話番号"
+          value={state.email}
+          onChange={handleChange} />
+      </label>
 
-        </div>
-        <input 
-        type="submit" 
-        value="Submit" 
-        className="clipboard-btn" 
+      <label>
+        電話番号:
+        <input
+          name="phone"
+          type="tel"
+          value={state.phone}
+          onChange={handleChange} />
+      </label>
+
+
+      <div id="note">
+        <ul>
+          <li>採取方法: {state.pickMethod}</li>
+          {
+            (state.pickMethod === '病院') &&
+            <li>病院名: {state.hospitalName}</li>
+          }
+          <li>ペットのお名前: {state.petName}</li>
+          <li>申込チューブの本数: {state.tubeNum}</li>
+          <li>氏名: {state.name}</li>
+          <li>住所: {state.address}</li>
+          <li>メールアドレス: {state.email}</li>
+          <li>電話番号: {state.phone}</li>
+        </ul>
+
+      </div>
+      <input
+        type="submit"
+        value="Submit"
+        className="clipboard-btn"
         data-clipboard-target="#note"
-        disabled={this.state.submitDisabled} />
-      </form>
-    );
-  }
+        disabled={state.submitDisabled} />
+    </form>
+  );
 }
 export default App;
